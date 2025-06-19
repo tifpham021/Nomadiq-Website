de1de453c80620a500170b27a3247fd22c2a9bca
tifpham021 <tpham346@gatech.edu>
2025-06-19 18:01:19 -0400

    added responsive features to home page

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,17 +1,25 @@
 import earthIcon from '../../assets/home-img/earth-icon.png';
 import '../navbar/navbar.css';
+import React, {useState} from 'react';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="navbar">
+    <nav className="navbar">
      <a href="/">
          <img src={earthIcon} draggable="false" alt="earth-icon" className="earth-icon"/>
      </a>
-      <div className="right">
+
+     <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </div>
+      
+      <div className={`right ${menuOpen ? 'open' : ''}`}>
         <a href = "#">Login</a>
         <a href = "#">Sign Up</a>
       </div>
-    </div>
+  
+    </nav>
   );
 };
 
