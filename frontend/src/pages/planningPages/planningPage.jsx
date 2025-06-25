09eb57b4cb1caae11d4f7239af3e1777fcc967da
tifpham021 <tpham346@gatech.edu>
2025-06-25 01:48:48 -0400

    finished the destination planning page + confirmation popup

--- frontend/src/pages/planningPages/planningPage.jsx
+++ frontend/src/pages/planningPages/planningPage.jsx
@@ -5,8 +5,39 @@ import plane from '../../assets/planning-img/plane.png';
 import train from '../../assets/planning-img/train.png';
 import bike from '../../assets/planning-img/other.png';
 import arrow from '../../assets/planning-img/arrow.png';
-const PlanningPage = () => {
+import Calendar from 'react-calendar';
+import React, {useState} from 'react';
+import 'react-calendar/dist/Calendar.css';
+import Popup from './popup.jsx';
 
+const PlanningPage = () => {
+    const [dateRange, setDateRange] = useState([new Date(), new Date()]);
+    const [arrivalDate, departureDate] = dateRange;
+    const [showPopup, setShowPopup] = useState(false);
+
+    const formatDateForInput = (date) => {
+        return date.toLocaleDateString('en-US', {
+            weekday: 'long',   
+            year: 'numeric',   
+            month: 'long',     
+            day: 'numeric'     
+        });
+    };
+
+    const handleInputChange = (e) => {
+        const newDate = new Date(e.target.value);
+        if (isNaN(newDate)) return;
+
+        if (type === 'arrival') {
+            setDateRange([newDate, departureDate]);
+        } else if (type === 'departure') {
+            setDateRange([arrivalDate, newDate]);
+    }
+
+    const confirmationPopup = () => {
+        
+    }
+    };
     return (
         <div
             className="planning-page-wrapper"
@@ -19,6 +50,7 @@ const PlanningPage = () => {
                             
                         }}
                         >
+            {showPopup && <Popup onClose={() => setShowPopup(false)} />}
             <div className='top-box'>
                 <h3>Your Destination:</h3>
                 <input placeholder="Where you're going..."/>
@@ -26,19 +58,30 @@ const PlanningPage = () => {
             <div className='inner-box'>
                 <div className='inner-box-items'>
                     <div className='calendar'>
-                        
+                        <Calendar
+                            prevLabel={<span>◀</span>}
+                            nextLabel={<span>▶</span>}
+                            next2Label={null}
+                            prev2Label={null}
+                            minDate={new Date()}
+                            maxDate={new Date(2030, 12, 31)}
+                            selectRange={true}
+                            value={dateRange}
+                            onChange={(range) => setDateRange(range)}
+                        />
                     </div>
                     <div className='dates'>
                         <h3>Date of Arrival</h3>
-                        <input type="date"/>
+                        <input value={formatDateForInput(arrivalDate)}
+                               />
                         <h3>Date of Departure</h3>
-                        <input type="date"/>
+                        <input value={formatDateForInput(departureDate)}
+                            />
                     </div>
                     <div className='confirm-details'>
-                        <h2>Confirm Details</h2>
-                        <div className='arrow-bg'>
-                            <img src={arrow}/>
-                        </div>
+                            <button className='arrow-bg' onClick={() => setShowPopup(true)}>
+                                <img src={arrow}/>
+                            </button>
                     </div>
                 </div>
             </div>
---
+++ frontend/src/pages/planningPages/popup.jsx
@@ -0,0 +1,25 @@
+import '../planningPages/popup.css';
+import image from  '../../assets/planning-img/popup.png';
+import {useNavigate} from 'react-router-dom';
+
+const Popup = ({onClose}) => {
+    const navigate = useNavigate()
+    return (
+        <div className='popup-overlay'>
+            <div className="popup">
+                <div className="popup-content">
+                    <div className='left'>
+                        <img src={image}/>
+                    </div>
+                    <div className='right'>
+                        <h2>Let's Start Planning for Your Trip to </h2>
+                        <h1>HAWAII </h1>
+                        <button className='lets-go' onClick={() => navigate('/weather')}>Let's Go</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Popup;
